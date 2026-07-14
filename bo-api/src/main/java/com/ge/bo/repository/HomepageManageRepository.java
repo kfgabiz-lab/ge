6f4f3ac7e8ed21ed05f74c70ad968d834dc7fc69
이병권 <kfgabiz@gmail.com>
2026-07-14 23:26:57 +0900

    chore: bo-api submodule 전환 및 서브모듈 포인터 반영

--- bo-api/src/main/java/com/ge/bo/repository/HomepageManageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ge.bo.repository;
-
-import com.ge.bo.entity.HomepageManage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HomepageManageRepository extends JpaRepository<HomepageManage, Long> {
-}
