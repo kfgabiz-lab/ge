f4c4e9926344663b432cb8062aef41e324efbcb3
이병권 <kfgabiz@gmail.com>
2026-07-14 23:21:22 +0900

    chore: fo submodule 전환 및 서브모듈 포인터 반영

--- fo/src/app/company/articles/page.tsx
+++
@@ -1,5 +0,0 @@
-import CompanyFeedPage from "@/app/company/components/CompanyFeedPage";
-
-export default function CompanyArticlesListPage() {
-  return <CompanyFeedPage variant="articles" />;
-}
--- fo/src/data/commonAssets.ts
+++
@@ -1,2 +0,0 @@
-/** 공통 empty state 아이콘 */
-export const emptyStateIconSrc = "/img/common/empty_icon.svg";
